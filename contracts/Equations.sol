1d6963335e412b68d84ecac66e43ec97177e19d4
syahirAmali <syahiramali@gmail.com>
2023-01-03 23:43:18 +0800

    Equations update

--- contracts/Equations.sol
+++ contracts/Equations.sol
@@ -14,26 +14,23 @@ contract Equations {
         NoOfEndorcements NE;
         QualityOfStakes QS;
         uint256 stakeAmount;
+        uint256 qualityStaker;
     }
 
     /// @notice Gets the reward per user
-    /// @param m multiplier = (how much extra from the stake ammount you want to give) - (approximately the upper limit of the MR as long as the Q of the stakers is high)
-    /// @param MR a struct of the params to get the value of quality amount for each user
-    /// @param weightage0 weightage of the number of endorcements
-    /// @param weightage1 weightage of the quality of stakes
-    /// @notice the weightage should be entered in basis points and equals to 10_000
+    /// @param maxRewards a struct of the params to get the value of quality amount for each user
     /// @param staker a struct of the params to get the value of the staker
-    function rewardPerUser(uint256 m, MaxRewards[] memory MR, uint256 weightage0, uint256 weightage1, StakerValue memory staker) public pure returns (uint256 rewards_) { 
-        
-        uint256 maxReward = getMaxRewards(m, MR, weightage0, weightage1);
-        
-        uint256 valueOfStaker = valueOfQualityAmount(weightage0, weightage1, staker.NE, staker.QS, staker.stakeAmount);
+    function rewardPerUser(uint256 maxRewards, MaxRewards[] memory MR, StakerValue memory staker) public pure returns (uint256 rewards_) { 
+        require(MR.length != 0, "!length");
 
-        uint256 sumOfStakers = getSumOfStakers(MR, weightage0, weightage1);
+        uint256 L = MR.length;
+        uint256 sumOfStakers_ = 0;
 
-        uint256 percent = _getPercent(valueOfStaker, sumOfStakers, 4);
+        for(uint256 i = 0; i < L; i++){
+            sumOfStakers_ += (MR[i].stakeAmount * MR[i].qualityStaker) / 100;
+        }
 
-        rewards_ = percent * maxReward / 10000;
+        rewards_ = (((staker.stakeAmount * staker.qualityStaker)/sumOfStakers_) * maxRewards ) / 100;
     }
 
     function _getPercent(
@@ -51,18 +48,28 @@ contract Equations {
         NoOfEndorcements NE;
         QualityOfStakes QS;
         uint256 stakeAmount;
+        uint256 qualityStaker;
     }
 
-    /// @notice gets the maximum reward for all of the stakers combined on a specific user
+    /// @notice Gets the sum of staker quality amount on a user
     /// @param m how much extra reward from the stake amount you want to give - approximately the upper limit of the maxRewards as long as the quality of the staker is high
-    /// @notice the m should be entered in basis points and equals to 10_000
-    /// @param MR a struct of the params to get the value of quality amount for each user
-    /// @param weightage0 weightage of the number of endorcements
-    /// @param weightage1 weightage of the quality of stakes
+    /// @param MR struct used to get the values relevant to find the sum of stakers and max rewards
     /// @notice the weightage should be entered in basis points and equals to 10_000
-    function getMaxRewards(uint256 m, MaxRewards[] memory MR, uint256 weightage0, uint256 weightage1) public pure returns (uint256 maxRewards_) {
-        uint256 sumOfStakers = getSumOfStakers(MR, weightage0, weightage1);
-        maxRewards_ = (sumOfStakers * m) / 10_000;
+    function getMaxRewards(uint256 m, MaxRewards[] memory MR, uint256 _availRewards) public pure returns (uint256 sumOfStakers_) {
+        require(MR.length != 0, "!length");
+
+        uint256 L = MR.length;
+        sumOfStakers_ = 0;
+
+        for(uint256 i = 0; i < L; i++){
+            sumOfStakers_ += (MR[i].stakeAmount * MR[i].qualityStaker) / 100;
+        }
+
+        sumOfStakers_ = (sumOfStakers_ * m) / 10_000;
+
+        if(_availRewards < sumOfStakers_){
+            sumOfStakers_ = _availRewards;
+        }
     }
 
     /// @notice Gets the sum of staker quality amount on a user
@@ -94,7 +101,6 @@ contract Equations {
     }
 
     struct NoOfEndorcements {
-        uint256 v;
         uint256 mE;
         uint256 eN;
     }
@@ -112,7 +118,7 @@ contract Equations {
     /// @param QS a struct of the params for getting the quality of stakes
     /// @return quality_ returns the quality of a staker
     function qualityOfStaker(uint256 weightage0, uint256 weightage1, NoOfEndorcements memory NE, QualityOfStakes memory QS) public pure returns (uint256 quality_){
-        quality_ = (((weightage0 * numberOfEndorcements(NE.v, NE.mE, NE.eN))) + ((weightage1 * qualityOfStakes(QS.cr, QS.cd)))) / 10000;
+        quality_ = (((weightage0 * numberOfEndorcements(NE.mE, NE.eN))) + ((weightage1 * qualityOfStakes(QS.cr, QS.cd)))) / 10000; // We multiply with weightage1 -> which is *10000, so we need to take it down again 
     }
     
     /// @notice determine the quality of stake
@@ -127,25 +133,19 @@ contract Equations {
         uint256 sum = 0;
 
         for(uint256 i = 0; i < N; i++){
-            //scaled up to 10_000
-            sum += cr[i] * 10_000  / cd[i] * 10_000;
+            sum += (cr[i] * 1000)  / (cd[i] * 10);
         }
 
-        // scaled down to get basis points or percentage, i.e 5000 = 50%
-        quality_ = (sum / N) / 10_000;
+        quality_ = (sum / N);
     }
 
     /// @notice calculate the number of endorcement points
-    /// @param v penalty number for small number of endorcements
     /// @param mE max number of endorcements
     /// @param eN number of endorcements
     /// @notice scaled to be 100 instead of one since theres not decimals
-    function numberOfEndorcements(uint256 v, uint256 mE, uint256 eN) public pure returns (uint256 number_) {
+    function numberOfEndorcements(uint256 mE, uint256 eN) public pure returns (uint256 number_) {
         require(eN < mE, "!eN");
 
-        number_ = ((100 + v) / (mE * 100)) * (eN*100);
-        if(number_ != 0){
-            number_ -= v;
-        }
+        number_ = (eN*100*100)/(mE*100);
     }
 }
