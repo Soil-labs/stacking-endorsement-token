7a7bc18d9e87319e0f9b9336a603b090c62e0b9a
syahirAmali <syahiramali@gmail.com>
2023-01-19 02:06:19 +0800

    Staking contract changes

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -2,16 +2,9 @@
 pragma solidity ^0.8.9;
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 interface IEquations {
-    struct MaxRewards {
-        NoOfEndorcements NE;
-        QualityOfStakes QS;
-        uint256 stakeAmount;
-        uint256 qualityStaker;
-    }
-
     struct NoOfEndorcements {
         uint256 mE;
         uint256 eN;
@@ -22,6 +15,13 @@ interface IEquations {
         uint256[] cd;
     }
 
+    struct MaxRewards {
+        NoOfEndorcements NE;
+        QualityOfStakes QS;
+        uint256 stakeAmount;
+        uint256 qualityStaker;
+    }
+
     function rewardPerUser(uint256 maxRewards, MaxRewards[] memory MR, MaxRewards memory staker) external view returns (uint256);
     function numberOfEndorcements(uint256 mE, uint256 eN) external view returns (uint256);
     function qualityOfStakes(uint256[] memory cr, uint256[] memory cd) external view returns (uint256);
@@ -29,7 +29,10 @@ interface IEquations {
     function getMaxRewards(uint256 m, MaxRewards[] memory MR, uint256 _availRewards) external view returns (uint256);
 }
 
-contract Staking is ReentrancyGuard {
+// TODO:  - available rewards // when a job is finished we enter the available rewards
+//        - withdraw stake
+
+contract Staking is Ownable {
     using SafeERC20 for IERC20;
 
     address public stakingToken;
@@ -38,55 +41,63 @@ contract Staking is ReentrancyGuard {
     uint256 public totalPools;
 
     struct UserPool {
-        // uint256 amountDeposited;
-        // mapping(address => uint256) stakerDepositedAmount;
-        // // mapping(address => uint256) stakerQuality;
-        // uint256 noOfStakers;
-        // mapping(uint256 => address) stakers;
         uint256 totalStaked; // CD
         uint256 weightage0;
         uint256 weightage1;
         uint256 multiplier;
 
         uint256 noOfUsersStaked;
-        address[] usersStaked;
+        mapping(uint256 => address) usersStaked;
+
+        address user;
     }
 
     struct Pool {
         bool endorced;
+        address poolUser;
         uint256 amountStaked; // CR
     }
 
     struct Staker {
         mapping(uint256 => Pool) pool;
         uint256 numberOfEndorcements;
+        uint[] poolsStaked;
     }
 
-    mapping(address => Staker) staker;
-    mapping(uint256 => UserPool) userPool;
+    mapping(address => Staker) public staker;
+    mapping(uint256 => UserPool) public userPool;
 
-    constructor(address _token, address _equations){
+    constructor(
+        address _token,
+        address _equations
+    ) {
         stakingToken = _token;
         equations = _equations;
     }
 
-    // TODO create only owner
-    function createPool(uint256 _weightage0, uint256 _weightage1, uint256 _multiplier) public {
+    // EXTERNAL FUNCTIONS
+
+    function createPool(
+        uint256 _weightage0,
+        uint256 _weightage1,
+        uint256 _multiplier,
+        address _user
+    ) external onlyOwner {
         uint256 currentPoolId = totalPools;
 
         userPool[currentPoolId].weightage0 = _weightage0;
         userPool[currentPoolId].weightage1 = _weightage1;
         userPool[currentPoolId].multiplier = _multiplier;
+        userPool[currentPoolId].user = _user;
 
         totalPools++;
     }
 
-    function staking() internal view returns (IERC20){
-        return IERC20(stakingToken);
-    }
-
-    function stakeOnPool(uint256 _poolId, uint256 _amount) public nonReentrant{
-        require(staking().balanceOf(msg.sender) > _amount, "!amount");
+    function stakeOnPool(
+        uint256 _poolId,
+        uint256 _amount
+    ) external {
+        require(_staking().balanceOf(msg.sender) > _amount, "!amount");
 
         Staker storage userData = staker[msg.sender];
         bool poolEndorced = userData.pool[_poolId].endorced;
@@ -96,8 +107,12 @@ contract Staking is ReentrancyGuard {
         if(!poolEndorced){
             poolEndorced = true;
             userData.numberOfEndorcements++;
+            userData.poolsStaked.push(_poolId);
 
             userPoolData.usersStaked[userPoolData.noOfUsersStaked] = msg.sender;
+
+            userData.pool[_poolId].poolUser = userPoolData.user;
+
             userPoolData.noOfUsersStaked++;
         }
 
@@ -105,56 +120,158 @@ contract Staking is ReentrancyGuard {
 
         userPoolData.totalStaked += _amount;
 
-        staking().safeTransferFrom(msg.sender, address(this), _amount);
+        _staking().safeTransferFrom(msg.sender, address(this), _amount);
     }
 
-    function _getNumberOfEndorcements(address _sender) internal view returns (uint256 NE_){
-        uint256 userEndorcementCount = staker[_sender].numberOfEndorcements;
-        NE_ = IEquations(equations).numberOfEndorcements(MAX_ENDORCEMENTS, userEndorcementCount);
+    // TODO: unstake, penalized for unstaking
+
+    // TODO: update job status of the user // onlyOwner/admin // simulate total rewards transfer
+
+    // TODO: staker total reward to be claimed
+
+    function claimRewardsFromPool(
+        uint256 _pool, 
+        address _staker
+    ) external {
+        require(msg.sender == _staker, "!staker");
+
+        uint256 rewards = getRewardPerUser(_pool, 1 ether, _staker);
+
+        _staking().safeTransfer(_staker, rewards);
     }
 
-    function _getQualityOfStakes(address _sender) internal view returns (uint256 QS_) {
+    function getPoolData(
+        uint256 _poolId
+    ) external view returns (
+        uint256 totalStaked_,
+        uint256 weightage0_,
+        uint256 weigthage1_,
+        uint256 multiplier_,
+        uint256 noOfUsersStaked_,
+        address user_,
+        address[] memory usersStaked_
+    ){
+        UserPool storage pool = userPool[_poolId];
+
+        totalStaked_ = pool.totalStaked;
+        weightage0_ = pool.weightage0;
+        weigthage1_ = pool.weightage1;
+        multiplier_ = pool.multiplier;
+        noOfUsersStaked_ = pool.noOfUsersStaked;
+        user_ = pool.user;
+
+        address[] memory staked = new address[](pool.noOfUsersStaked);
+
+        for(uint256 i = 0; i < pool.noOfUsersStaked; ++i){
+            staked[i] = pool.usersStaked[i];
+        }
+
+        usersStaked_ = staked;
+    }
+
+    function getStakerData(
+        address _staker
+    ) external view returns (
+        uint256 numberOfEndorcements_,
+        Pool[] memory pools
+    ){
+        Staker storage stakerData = staker[_staker];
+
+        numberOfEndorcements_ = stakerData.numberOfEndorcements;
+
+        Pool[] memory myPools = new Pool[](stakerData.poolsStaked.length);
+
+        for(uint i = 0; i < stakerData.poolsStaked.length; ++i){
+            myPools[i] = (stakerData.pool[stakerData.poolsStaked[i]]);
+        }
+
+        pools = myPools;
+    }
+
+    function getNumberOfEndorcements(
+        address _staker
+    ) external view returns (
+        uint256 NE_
+    ){
+        NE_ = _getNumberOfEndorcements(_staker);
+    }
+
+    function getQualityOfStakes(
+        address _sender
+    ) external view returns (
+        uint256 QS_
+    ){
         Staker storage userData = staker[_sender];
-        uint256[] memory cr = new uint256[](totalPools);
+        uint256[] memory cr = new uint256[](userData.poolsStaked.length);
 
-        for(uint256 i = 0; i < totalPools; ++i){
+        for(uint256 i = 0; i < userData.poolsStaked.length; ++i){
             cr[i] = userData.pool[i].amountStaked;
         }
 
-        uint256[] memory cd = new uint256[](totalPools);
+        uint256[] memory cd = new uint256[](userData.poolsStaked.length);
 
-        for(uint256 i = 0; i < totalPools; ++i){
+        for(uint256 i = 0; i < userData.poolsStaked.length; ++i){
             cd[i] = userPool[i].totalStaked;
         }
 
         QS_ = IEquations(equations).qualityOfStakes(cr, cd);
     }
 
-    // TODO: create external versions of these 2 functions
+    // PUBLIC FUNCTIONS
 
-    // function _getNumberOfEndorcements(address _sender) internal view returns (uint256 NE_){
-    //     uint256 userEndorcementCount = staker[_sender].numberOfEndorcements;
-    //     NE_ = IEquations(equations).numberOfEndorcements(MAX_ENDORCEMENTS, userEndorcementCount);
-    // }
+    function getQualityOfStaker(
+        uint256 _poolId,
+        address _user
+    ) public view returns (
+        uint256 qualityOfStaker_
+    ){
+        qualityOfStaker_ = _getQualityOfStaker(_poolId, _user);
+    }
 
-    // function _getQualityOfStakes(address _sender) internal view returns (uint256 QS_) {
-    //     Staker storage userData = staker[_sender];
-    //     uint256[] memory cr = new uint256[](totalPools);
+    function getMaxRewards(
+        uint256 _poolId,
+        uint256 _availableRewards
+    ) public view returns (
+        uint256 maxRewards_
+    ) {
+        IEquations.MaxRewards[] memory MR;
+        uint256 multiplier;
+        (MR, multiplier) = _getMaxRewardsStruct(_poolId);
 
-    //     for(uint256 i = 0; i < totalPools; ++i){
-    //         cr[i] = userData.pool[i].amountStaked;
-    //     }
+        maxRewards_ = IEquations(equations).getMaxRewards(multiplier, MR, _availableRewards);
+    }
 
-    //     uint256[] memory cd = new uint256[](totalPools);
+    function getRewardPerUser(
+        uint256 _poolId,
+        uint256 _availableRewards,
+        address _staker
+    ) public view returns (
+        uint256 rewards_
+    ){
+        uint256 maxRewards = getMaxRewards(_poolId, _availableRewards);
+        IEquations.MaxRewards[] memory MR;
+        (MR,) = _getMaxRewardsStruct(_poolId);
+        IEquations.MaxRewards memory stakerValue = _getStakerStruct(_poolId, _staker);
+        rewards_ = IEquations(equations).rewardPerUser(maxRewards, MR, stakerValue);
+    }
 
-    //     for(uint256 i = 0; i < totalPools; ++i){
-    //         cd[i] = userPool[i].totalStaked;
-    //     }
+    // INTERNAL FUNCTIONS
 
-    //     QS_ = IEquations(equations).qualityOfStakes(cr, cd);
-    // }
+    function _staking() internal view returns (IERC20){
+        return IERC20(stakingToken);
+    }
 
-    function _getQualityOfStaker(uint256 _poolId, address _user) internal view returns (uint256 qualityOfStaker_) {
+    function _getNumberOfEndorcements(address _staker) internal view returns (uint256 NE_){
+        uint256 userEndorcementCount = staker[_staker].numberOfEndorcements;
+        NE_ = IEquations(equations).numberOfEndorcements(MAX_ENDORCEMENTS, userEndorcementCount);
+    }
+
+    function _getQualityOfStaker(
+        uint256 _poolId,
+        address _user
+    ) internal view returns (
+        uint256 qualityOfStaker_
+    ){
         IEquations.NoOfEndorcements memory NE;
         NE.mE = MAX_ENDORCEMENTS;
         NE.eN = staker[_user].numberOfEndorcements;
@@ -163,10 +280,10 @@ contract Staking is ReentrancyGuard {
 
         Staker storage userData = staker[_user];
 
-        uint256[] memory cr = new uint256[](totalPools);
-        uint256[] memory cd = new uint256[](totalPools);
+        uint256[] memory cr = new uint256[](userData.poolsStaked.length);
+        uint256[] memory cd = new uint256[](userData.poolsStaked.length);
 
-        for(uint256 j = 0; j < totalPools; ++j){
+        for(uint256 j = 0; j < userData.poolsStaked.length; ++j){
             cr[j] = userData.pool[j].amountStaked;
             cd[j] = userPool[j].totalStaked;
 
@@ -178,61 +295,32 @@ contract Staking is ReentrancyGuard {
         qualityOfStaker_ = IEquations(equations).qualityOfStaker(userPool[_poolId].weightage0, userPool[_poolId].weightage1, NE, QS);
     }
 
-    function getQualityOfStaker(uint256 _poolId) public view returns (uint256 qualityOfStaker_) {
-        IEquations.NoOfEndorcements memory NE;
-        NE.mE = MAX_ENDORCEMENTS;
-        NE.eN = staker[msg.sender].numberOfEndorcements;
-
-        IEquations.QualityOfStakes memory QS;
-
-        Staker storage userData = staker[msg.sender];
-
-        uint256[] memory cr = new uint256[](totalPools);
-        uint256[] memory cd = new uint256[](totalPools);
-
-        for(uint256 j = 0; j < totalPools; ++j){
-            cr[j] = userData.pool[j].amountStaked;
-            cd[j] = userPool[j].totalStaked;
-
-        }
-
-        QS.cr = cr;
-        QS.cd = cd;
-
-        qualityOfStaker_ = IEquations(equations).qualityOfStaker(userPool[_poolId].weightage0, userPool[_poolId].weightage1, NE, QS);
-    }
-
-    function _getMaxRewards(uint256 _poolId) internal view returns (IEquations.MaxRewards[] memory MR_) {
-        IEquations.MaxRewards[] memory MR;
-
+    function _getMaxRewardsStruct(
+        uint256 _poolId
+    ) internal view returns (
+        IEquations.MaxRewards[] memory MR_,
+        uint256 multiplier_
+    ){
         UserPool storage userPoolData = userPool[_poolId];
         uint256 noOfStakers = userPoolData.noOfUsersStaked;
 
+        IEquations.MaxRewards[] memory MR = new IEquations.MaxRewards[](noOfStakers);
+
         for(uint256 i = 0; i < noOfStakers; ++i){
             address user = userPoolData.usersStaked[i];
-            MR[i] = getStakerStruct(_poolId, user);
+            MR[i] = _getStakerStruct(_poolId, user);
         }
 
         MR_ = MR;
+        multiplier_ = userPoolData.multiplier;
     }
 
-    function getMaxRewards(uint256 _poolId, uint256 _availableRewards) public view returns (uint256 maxRewards_) {
-        IEquations.MaxRewards[] memory MR;
-
-        UserPool storage userPoolData = userPool[_poolId];
-        uint256 noOfStakers = userPoolData.noOfUsersStaked;
-        uint256 multiplier = userPoolData.multiplier;
-
-        for(uint256 i = 0; i < noOfStakers; ++i){
-            address user = userPoolData.usersStaked[i];
-            
-            MR[i] = getStakerStruct(_poolId, user);
-        }
-
-        maxRewards_ = IEquations(equations).getMaxRewards(multiplier, MR, _availableRewards);
-    }
-
-    function getStakerStruct(uint256 _poolId, address _user) internal view returns (IEquations.MaxRewards memory MR_){
+    function _getStakerStruct(
+        uint256 _poolId,
+        address _user
+    ) internal view returns (
+        IEquations.MaxRewards memory MR_
+    ){
         IEquations.MaxRewards memory MR;
 
         IEquations.NoOfEndorcements memory NE;
@@ -243,94 +331,26 @@ contract Staking is ReentrancyGuard {
 
         Staker storage userData = staker[_user];
 
-        uint256[] memory cr = new uint256[](totalPools);
-        uint256[] memory cd = new uint256[](totalPools);
+        uint256[] memory cr = new uint256[](userData.poolsStaked.length);
+        uint256[] memory cd = new uint256[](userData.poolsStaked.length);
 
-        for(uint256 j = 0; j < totalPools; ++j){
+        for(uint256 j = 0; j < userData.poolsStaked.length; ++j){
             cr[j] = userData.pool[j].amountStaked;
             cd[j] = userPool[j].totalStaked;
-
         }
 
         QS.cr = cr;
         QS.cd = cd;
-    
-        uint256 stakeAmount = userData.pool[_poolId].amountStaked;
 
+        uint256 stakeAmount = userData.pool[_poolId].amountStaked;
         uint256 qualityStaker = _getQualityOfStaker(_poolId, _user);
 
         MR.NE = NE;
         MR.QS = QS;
+
         MR.stakeAmount = stakeAmount;
         MR.qualityStaker = qualityStaker;
 
         MR_ = MR;
     }
-
-    function getRewardPerUser(uint256 _poolId, uint256 _availableRewards) public view returns (uint256 rewards_) {
-        uint256 maxRewards = getMaxRewards(_poolId, _availableRewards);
-
-        IEquations.MaxRewards[] memory MR = _getMaxRewards(_poolId);
-
-        IEquations.MaxRewards memory stakerValue = getStakerStruct(_poolId, msg.sender);
-
-        rewards_ = IEquations(equations).rewardPerUser(maxRewards, MR, stakerValue);
-    }
-
-    function getRewardPerUser2(uint256 _poolId, uint256 _availableRewards) public view returns (uint256 rewards_) {
-        uint256 maxRewards = getMaxRewards(_poolId, _availableRewards);
-
-        IEquations.MaxRewards[] memory MR = _getMaxRewards(_poolId);
-
-        IEquations.MaxRewards memory stakerValue;
-
-        IEquations.NoOfEndorcements memory NE;
-        NE.mE = MAX_ENDORCEMENTS;
-        NE.eN = staker[msg.sender].numberOfEndorcements;
-
-        IEquations.QualityOfStakes memory QS;
-
-        Staker storage userData = staker[msg.sender];
-
-        uint256[] memory cr = new uint256[](totalPools);
-        uint256[] memory cd = new uint256[](totalPools);
-
-        for(uint256 j = 0; j < totalPools; ++j){
-            cr[j] = userData.pool[j].amountStaked;
-            cd[j] = userPool[j].totalStaked;
-
-        }
-
-        QS.cr = cr;
-        QS.cd = cd;
-    
-        uint256 stakeAmount = userData.pool[_poolId].amountStaked;
-
-        uint256 qualityStaker = _getQualityOfStaker(_poolId, msg.sender);
-
-        stakerValue.NE = NE;
-        stakerValue.QS = QS;
-        stakerValue.stakeAmount = stakeAmount;
-        stakerValue.qualityStaker = qualityStaker;
-
-        rewards_ = IEquations(equations).rewardPerUser(maxRewards, MR, stakerValue);
-    }
-
-    // function withdrawStakeOnPool(uint256 _pool, uint256 _amount) public nonReentrant {
-    //     Pool storage selectedPool = pool[_pool];
-    //     uint256 balance = selectedPool.stakerDepositedAmount[msg.sender];
-
-    //     require(balance > _amount, "!balance");
-
-    //     selectedPool.amountDeposited -= _amount;
-    //     selectedPool.stakerDepositedAmount[msg.sender] -= _amount;
-
-    //     staking().safeTransfer(msg.sender, _amount);
-
-    //     // TODO get all of the users in a particular pool
-    //     // uint256 rewards = IEquations(equations).rewardPerUser(m, MR, weightage0, weightage1, staker);
-
-    //     // TODO then reward tokens are transffered from where?
-    // }
-
 }
