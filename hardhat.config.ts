e796d4d2c89b1ad16c0411bfc21cd716f0463b2f
syahirAmali <syahiramali@gmail.com>
2023-01-19 02:04:56 +0800

    Added gas reporter

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,13 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+  },
 };
+
+
 
 export default config;
