a73e32a3740ce12359654c0a88429174ea1be716
syahirAmali <syahiramali@gmail.com>
2023-02-09 14:06:46 +0800

    netowrk config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,12 +2,28 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  networks: {
+    hardhat: {
+      forking: {
+        url: "https://matic-mumbai.chainstacklabs.com",
+      },
+      chainId: 80001,
+    },
+
+    mumbai: {
+      url: "https://matic-mumbai.chainstacklabs.com",
+      chainId: 80001,
+      accounts: [
+        "",
+      ],
+    },
+  },
 
   gasReporter: {
     enabled: true,
     currency: "USD",
   },
+
 };
 
 
